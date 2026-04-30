e68a05db6a266471a41977486f603827ed66d684
Andrea_Pasapera <andrea.pasapera@educa.madrid.org>
2026-04-30 11:00:48 +0200

    "Falta por hacer el ejercicio 05 de ArrayList"

---
+++ src/Ejercicios/Ejercicio05.java
@@ -0,0 +1,13 @@
+package Ejercicios;
+/**
+ * Nuevo ejercicio de la colección de discos pero 
+ * usando esta vez una lista para almacenar la informaiçon sobre los discos
+ * en lugar de un array convencional. 
+ * 
+ * @author Andrea Pasapera
+ */
+public class Ejercicio05 {
+    public static void main(String[] args) {
+        
+    }
+}
